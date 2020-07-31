051800fa93a11afd974394754ff44a8d7fae927f
Tung Huynh <tunghuynh314@gmail.com>
2020-07-30 22:09:33 -0500

    Update Pages

---
+++ about.md
@@ -0,0 +1,7 @@
+---
+layout: page
+title: About
+permalink: /about/
+---
+
+I am a software engineer and a researcher. My interests includes the Mind and the Universe.
--- install.sh
+++
@@ -1,7 +0,0 @@
-brew install libxml2 libxslt libiconv
-export NOKOGIRI_USE_SYSTEM_LIBRARIES=1
-
-brew reinstall libffi
-export LDFLAGS="-L/usr/local/opt/libffi/lib" && \
-export PKG_CONFIG_PATH="/usr/local/opt/libffi/lib/pkgconfig" && \
-bundle install
